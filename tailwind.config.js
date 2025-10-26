122c0d1208072aa96b5a5da6cdbc5e4c583c5d0a
Nelson Londoño <nelsonlondonodev@gmail.com>
2025-10-26 07:09:22 +0100

    Update Tailwind CSS configuration to specify content paths for HTML and JS files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/*.{html,js}"],
+  content: ["./index.html", "./blog/**/*.html", "./js/**/*.js"],
   darkMode: 'class',
   theme: {
     extend: {
