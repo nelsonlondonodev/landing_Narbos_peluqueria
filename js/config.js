e1867f0b4951b80719c009204da4d0893d1cd580
Nelson Londoño <nelsonlondonodev@gmail.com>
2026-05-25 07:46:32 +0200

    chore: bump project version to v2.8.8 and update cache busters

--- js/config.js
+++ js/config.js
@@ -98,7 +98,7 @@ export const resolveAsset = (path, prefix = '') => {
  * Configuración global del sitio.
  */
 export const siteConfig = Object.freeze({
-    version: "2.8.7", // Versión actualizada
+    version: "2.8.8", // Versión actualizada
     basePath: BASE_PATH,
     socialLinks: [
         {
