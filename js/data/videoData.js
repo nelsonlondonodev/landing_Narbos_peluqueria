61ab7081116cfb94d5752c9862ea90f8b9b15e7e
Nelson Londoño <nelsonlondonodev@gmail.com>
2026-06-17 07:19:58 +0200

    feat: add new YouTube Shorts video to gallery and update JSON-LD schema

--- js/data/videoData.js
+++ js/data/videoData.js
@@ -4,5 +4,6 @@
  */
 export const homeVideos = [
     'sz-uA1RgpVs', // Transformación de Color y Balayage en Chía
-    'MC-SPNzjk0E'  // Transformación de Estilo y Color (Shorts)
+    'MC-SPNzjk0E', // Transformación de Estilo y Color (Shorts)
+    'ImN8W2AXEJI'  // Corte y Estilo de Vanguardia (YouTube Shorts) - Última publicación
 ];
